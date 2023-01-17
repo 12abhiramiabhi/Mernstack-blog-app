19c89315a95c26e090830b79bcb558955fda27d5
abhirami <abhiabhirami972@gmail.com>
2023-01-17 20:50:07 +0530

    completed login pagee

--- front-end/src/Components/SignupPage.jsx
+++ front-end/src/Components/SignupPage.jsx
@@ -6,10 +6,10 @@ function SignupPage() {
     return (
         <div className='signup-container2'>
             <div className='signup-card'>
-                <h1 className='signup-head'> signup</h1><br />
+                <h1 className='signup-head'> Signup</h1><br />
                 <input type="text" placeholder='name' className='input12' /><br />
                 <input type="password" placeholder='password' className=' input1' /><br />
-                <input type="text" placeholder='enter email' className='input2' /><br />
+                <input type="text" placeholder='email' className='input2' /><br />
                 <input type="text" placeholder=' phone number' className='input3' /><br />
                 <button className='signup-button'>signup</button>
             </div>
