16ad6e423c8d5736a587cc12d6e8e5800ff3f9a8
abhirami <abhiabhirami972@gmail.com>
2023-01-22 20:26:59 +0530

    completed homepage css..

--- front-end/src/Components/HomePage.jsx
+++ front-end/src/Components/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
           <header>
 <nav className='navbar'>
 <div className='container'>
-    <a href="HomePage.jsx" className='navbar-brand'>Art design </a>
+    <a href="HomePage.jsx" className='navbar-brand'>Art design</a>
 <div className='navbar-nav'>
     <a href="">home</a>
     <a href="">blog</a>
@@ -23,11 +23,11 @@ function HomePage() {
 <div className='banner'>
 <div className='container'>
     <h1 className='banner-title'>
-        <span>art.</span>design
+        <span>art.</span>design blog
     </h1>
     <p>everything that you want to know about art & design</p>
     <form>
-        <input type="text" className='search-input' placeholder='find your food .  .  .' />
+        <input type="text" className='search-input' placeholder='search ...' />
         <button type='submit' className='search-btn'>
             <i className='fas fa-search'></i>
         </button>
@@ -49,7 +49,7 @@ function HomePage() {
     <div className='design-item'>
         <div className='design-img'>
             <img src="https://media.istockphoto.com/id/1132442970/photo/colored-powder-explosion-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=9f6Nc2aWd_D8tQRvA3smOJxuG8P9YBTbSx3At4OEqyk=" alt="" />
-            <span><i className='far fa-heart'></i>22</span>
+            <span><i className='far fa-heart'></i>like 22</span>
             <span>art $ design </span>
         </div>
         <div className='design-title'>
@@ -59,7 +59,7 @@ function HomePage() {
     <div className='design-item'>
         <div className='design-img'>
             <img src="https://c4.wallpaperflare.com/wallpaper/658/185/126/squirt-art-guy-wallpaper-preview.jpg" alt="" />
-            <span><i className='far fa-heart'></i>22</span>
+            <span><i className='far fa-heart'></i>like 22</span>
             <span>art $ design </span>
         </div>
         <div className='design-title'>
@@ -69,7 +69,7 @@ function HomePage() {
     <div className='design-item'>
         <div className='design-img'>
             <img src="https://t3.ftcdn.net/jpg/05/24/43/88/360_F_524438877_PZFzwc5OWJ3MTWQVFfHKwu1DRVMaSgPx.jpg" alt="" />
-            <span><i className='far fa-heart'></i>22</span>
+            <span><i className='far fa-heart'></i>like 22</span>
             <span>art $ design </span>
         </div>
         <div className='design-title'>
@@ -79,7 +79,7 @@ function HomePage() {
     <div className='design-item'>
         <div className='design-img'>
             <img src="https://media.istockphoto.com/photos/hand-painted-background-with-mixed-liquid-blue-and-golden-paints-picture-id1162167657?b=1&k=20&m=1162167657&s=612x612&w=0&h=6ZSIaRaxMptYS8zniEUAB3gAr2EvuBX2LK-fYua0okU=" alt="" />
-            <span><i className='far fa-heart'></i>22</span>
+            <span><i className='far fa-heart'></i>like 22</span>
             <span>art $ design </span>
         </div>
         <div className='design-title'>
@@ -89,7 +89,7 @@ function HomePage() {
     <div className='design-item'>
         <div className='design-img'>
             <img src="https://media.istockphoto.com/id/1134512518/photo/abstract-hand-painted-art-background-on-canvas.jpg?b=1&s=612x612&w=0&k=20&c=UfkeC1cZHV-jAB1pUEg5zdr_0wMv32eZWDbPTQn6748=" alt="" />
-            <span><i className='far fa-heart'></i>22</span>
+            <span><i className='far fa-heart'></i>like 22</span>
             <span>art $ design </span>
         </div>
         <div className='design-title'>
