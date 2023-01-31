e84be341be78f0780855e38e0f807286deffe664
abhirami <abhiabhirami972@gmail.com>
2023-01-31 22:13:30 +0530

    completed singleblogpage2

--- front-end/src/Components/SingleBlogPage.jsx
+++ front-end/src/Components/SingleBlogPage.jsx
@@ -2,16 +2,15 @@ import './singleblogpage.css'
 
 function SingleBlogPage() {
     return (
-        <div className="single-card">
-            <div className='signgle-box' >
-                <h1 className='single-title'>title</h1>
-                <p className='para-lorem' >Lorem ipsum dolor sit amet consectetur adipisicing elit. A, voluptatem laborum amet impedit illo cumque mollitia nobis facilis veritatis nisi perferendis deleniti consectetur aperiam, quibusdam deserunt, aspernatur praesentium beatae asperiores.
-
-
-                </p>
+      
+           <div className='signup-container2'>
+            <div className='signup-card'>
+            <h1 className='title-head' >Title</h1>
+<p className='para9'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis deserunt sequi delectus odit itaque architecto illo perferendis doloremque corporis incidunt, labore reiciendis ullam cum doloribus! Esse pariatur magni quisquam dolorem!</p> 
+            
             </div>
-
         </div>
+
     )
 }
 
