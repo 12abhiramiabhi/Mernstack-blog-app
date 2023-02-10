800c527b770fa95ebcd8ffacf9b385d85d23b59e
abhirami <abhiabhirami972@gmail.com>
2023-02-10 16:27:16 +0530

    completed cardblog2

--- front-end/src/Components/CategaryBlog.jsx
+++ front-end/src/Components/CategaryBlog.jsx
@@ -29,8 +29,8 @@ function CategaryBlog() {
     </div>
     <div className="container-cate">
       <div className="cate-card">
-        <h1>categaryblog</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi ratione doloremque dolorem ab deserunt quod impedit, in adipisci, suscipit asperiores sed. Reiciendis consectetur accusantium repellendus
+        <h1 className="cat-head">categaryblog</h1>
+        <p className="para8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi ratione doloremque dolorem ab deserunt quod impedit, in adipisci, suscipit asperiores sed. Reiciendis consectetur accusantium repellendus
           
         </p>
       </div>
