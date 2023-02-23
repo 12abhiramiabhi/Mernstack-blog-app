db59abc2b372629c1396f63137dcc8e5721f6f71
Poornima Murali <poornimamuralip@gmal.com>
2023-02-23 15:15:08 +0530

    completed blogpage

--- front-end/src/Components/CmntCard.jsx
+++ front-end/src/Components/CmntCard.jsx
@@ -6,7 +6,7 @@ function CmntCard({ blogCmntss }) {
             <div className="viewcmnt">
                 <div className="cmnt_read">
                     <h3 className='user-name'>{blogCmntss.userName}</h3>
-                    <p className='cmnt-para' >{blogCmntss.comntText}</p>
+                    <p className='cmnt-para' >{blogCmntss.CommendText}</p>
                     <button className='view-cmnt' >{blogCmntss.date}</button>
                 </div>
             </div>
--- front-end/src/Components/Edit.jsx
+++ front-end/src/Components/Edit.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
-import "./BlogEditing.css"
+import "./edit.css"
 
 function Edit() {
 
